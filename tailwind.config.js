3b62c70317f7d5905f0233d70c034803a75f368b
LeemoTseng-Mac <leemotseng@LeeMo-Tseng.local>
2025-03-31 12:18:47 +0800

    init

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#f2f9fb',
+          100: '#c2e3ea',
+          200: '#91ceda',
+          300: '#61b8c9',
+          400: '#30a2b9',
+          500: '#008ca8',
+          600: '#00778f',
+          700: '#006276',
+          800: '#004d5c',
+          900: '#003843',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
